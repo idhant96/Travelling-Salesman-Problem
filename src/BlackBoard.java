063fa74e08771d8203a1eaf01aa7505739cbb6f3
Rahul Suresh <rsures13@asu.edu>
2020-11-18 10:48:01 -0700

    added the blackboard skeleton

---
+++ src/BlackBoard.java
@@ -0,0 +1,46 @@
+import java.util.List;
+
+public class BlackBoard extends Observable{
+    DataPoints datapoints;
+    TspShortestPaths tspShortestPaths;
+    Map<Integer,TspPath> startToTsppath;
+
+    private BlackBoard() {
+
+    }
+    public BlackBoard getBlackBoardInstance() {
+        return null;
+    }
+
+    public void addCoordinatesToDataPoints(){
+
+    }
+
+    public DataPoints getDatapoints(){
+        return null;
+
+    }
+
+    public void getTspShortestPaths(){
+
+    }
+
+    public void cleanSlate(){
+
+    }
+
+    public TspPath getTspPathForStartCity(){
+        return null;
+    }
+
+    public List<TspPath> getTspPaths(){
+        return null;
+
+    }
+
+    public void update(){
+
+    }
+
+
+}
