abbf254b950119de4b452cfd0656652eb3259dc9
akrish84 <akrish84@asu.edu>
2020-11-18 19:22:27 -0700

    Adding length function to DataPoints

--- src/DataPoints.java
+++ src/DataPoints.java
@@ -5,12 +5,16 @@ public class DataPoints {
 
     }
 
-    public int getDistance() {
+    public int getDistance(int start, int end) {
         return 0;
 
     }
 
     public int[][] getCoordinates() {
         return null;
+    }
+    
+    public int getLength() {
+    	return coordinates.length;
     }
 }
--- src/NearestNeighbor.java
+++ src/NearestNeighbor.java
@@ -1,13 +1,23 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NearestNeighbor {
 	
 	
 	public void calculateNextNearestNeighbor(TspPath tspPath) {
 		
+		DataPoints dataPoints = BlackBoard.getInstance().getDatapoints();
+		Set<Integer> visited = new HashSet<>(tspPath.getPath());
+		int lastVisited = tspPath.getPath().get(tspPath.getPath().size()-1);
 		
-		List<Integer> visited = tspPath.getPath();
-		int lastVisited = visited.get(visited.size()-1);
+		int minDistance = Integer.MAX_VALUE;
+		int minCity = Integer.MAX_VALUE;
+		for(int i = 0 ; i < dataPoints.coordinates.length; i++) {
+			if(visited.contains(i)) {
+				continue;
+			}
+		}
 		
 		
 	}
