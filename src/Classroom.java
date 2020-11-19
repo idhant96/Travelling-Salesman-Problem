10b7aedf7cba8f7121fe9edb9ae7b963910527fa
idhant96 <idhant123@gmail.com>
2020-11-19 10:01:24 -0700

    classroom organized

--- src/Classroom.java
+++ src/Classroom.java
@@ -2,41 +2,59 @@ import java.util.ArrayList;
 
 public class Classroom {
 	private int studentCount=5;
-	private ArrayList<Thread> studentThreads;
+//	private ArrayList<Thread> studentThreads; 
 	private ArrayList<Student> students;
 	private Professor professor;
-	private Thread professorThread;
+//	private Thread professorThread;
 	
-	private void prepareThreads() {
-		if (studentThreads == null) {
-			studentThreads = new ArrayList<Thread>();
-			students = new ArrayList<Student>();
-			Student student;
-			for (int i=0;i<studentCount; i++) {
-				student = new Student();
-				students.add(student);
-				studentThreads.add(new Thread(student));
-			}
-		}
-		if (professorThread == null) {
-			professor = new Professor();
-			professorThread = new Thread(professor);
-		} 
-	}
+//	private void prepareThreads() {
+//		if (studentThreads == null) {
+//			studentThreads = new ArrayList<Thread>();
+//			students = new ArrayList<Student>();
+//			Student student;
+//			for (int i=1;i<=studentCount; i++) {
+//				student = new Student(i, studentCount);
+//				students.add(student);
+//				studentThreads.add(new Thread(student));
+//			}
+//		}
+//		if (professorThread == null) {
+//			professor = new Professor();
+//			professorThread = new Thread(professor);
+//		} 
+//	}
 	
 	
 	public void start() {
-		prepareThreads();
+//		prepareThreads();
 		Thread tempThread;
-		Student student;
-		for (int i=0;i<studentCount; i++) {
-			tempThread = studentThreads.get(i);
-			student = students.get(i);
-			student.startCompute();
-			tempThread.start();
+		if (students.isEmpty()) {
+//			studentThreads = new ArrayList<Thread>();
+			students = new ArrayList<Student>();
+			Student student;
+			for (int i=1;i<=studentCount; i++) {
+				student = new Student(i, studentCount);
+				students.add(student);
+				tempThread = new Thread(student);
+				tempThread.start();
+//				studentThreads.add(new Thread(student));
+			}
 		}
-		professor.startCompute();
-		professorThread.start();
+		if (professor == null) {
+			professor = new Professor();
+			tempThread = new Thread(professor);
+			tempThread.start();
+		} 
+		
+//		Student student;
+//		for (int i=0;i<studentCount; i++) {
+//			tempThread = studentThreads.get(i);
+//			student = students.get(i);
+//			student.startCompute();
+//			tempThread.start();
+//		}
+//		professor.startCompute();
+//		professorThread.start();
 	}
 	
 //	public void pause() {
@@ -53,15 +71,17 @@ public class Classroom {
 	public void stop() {
 		Thread tempThread;
 		Student student;
-		for (int i=0;i<studentCount; i++) {
-			tempThread = studentThreads.get(i);
-			student = students.get(i);
-			student.stopCompute();
-			student = null;
+		if (!students.isEmpty()) {
+			for (int i=0;i<studentCount; i++) {
+				student = students.get(i);
+				student.stopCompute();
+//				student = null;
+			}
 		}
-		studentThreads = null;
-		professor.stopCompute();
-		professor = null;
+		
+		students.clear();
+//		professor.stopCompute();
+//		professor = null;
 	}
 	
 	
@@ -76,12 +96,12 @@ public class Classroom {
 //        }
 //		classroom.stop();
 //		
-//		try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//		classroom.start(); // works 
+////		try {
+////            Thread.sleep(10);
+////        } catch (InterruptedException e) {
+////            e.printStackTrace();
+////        }
+////		classroom.start(); // works 
 //		
 //	}
 	
